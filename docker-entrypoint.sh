12d8627d8bc9f03487b886a481bb458176466ac9
seffeng <seffeng@sina.cn>
2021-06-10 09:48:26 +0800

    test:update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ init_database_dir() {
         mkdir -p ${BASE_DIR}/data/mysql
         chown -R mysql:mysql ${BASE_DIR}/data/mysql ${BASE_DIR}/logs ${BASE_DIR}/tmp
         mysqld --initialize-insecure --user=mysql --datadir=${BASE_DIR}/data/mysql
-        bin/mysql_ssl_rsa_setup --datadir=${BASE_DIR}/data/mysql
+        mysql_ssl_rsa_setup --datadir=${BASE_DIR}/data/mysql
     fi
 }
 
